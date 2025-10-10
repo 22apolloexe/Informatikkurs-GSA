a0a88edb0294852bf9ae02f32200e0d258bbec97
999bogi <b.karachanski@gmail.com>
2025-10-10 19:38:13 +0200

    Delete Kapitel 3/Lösungen directory

--- Kapitel 3/Lösungen/Aufgabe3.java
+++
@@ -1,9 +0,0 @@
-public class Aufgabe3 {
-  public static void main(String[] args) {
-    long maxLong = (long)Math.pow(2,63) - 1;   // 2^63 ist als double exakt, -1 ergibt Long-Maximum
-    long minLong = - (long)Math.pow(2,63); 
-    
-    System.out.println("Min long = " + minLong); // Ergebnis: -9223372036854775808
-    System.out.println("Max long = " + maxLong); // Ergebnis: 9223372036854775807
-  }
-}
